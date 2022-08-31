3bdafcc151659fb389ee8068f8198851b720710b
Jun Lin <linjunpop@gmail.com>
2022-08-31 14:49:35 +0800

    Fixes typo.

--- config/runtime.exs
+++ config/runtime.exs
@@ -35,7 +35,7 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :slip, SlipWeb.Endpoint,
+  config :flowr, FlowrWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
