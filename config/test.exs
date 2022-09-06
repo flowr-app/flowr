0076a021650b61f31726f8c9b0a29e539672cad3
Jun Lin <linjunpop@gmail.com>
2022-09-06 16:29:42 +0800

    Added `secret_key_base` back to test config file.

--- config/test.exs
+++ config/test.exs
@@ -6,13 +6,18 @@ config :flowr, Flowr.Repo,
   password: "postgres",
   database: "flowr_test",
   hostname: System.get_env("DB_HOST") || "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :flowr, FlowrWeb.Endpoint,
   http: [port: 4002],
+  secret_key_base: "fjkOJRyp5sz09GNzxkW7yKlPxVIKzu8LPhq9sb3Ftbg0tnmhcVfBn0Ijp+xswt1T",
   server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
